5ea9fc7fc4a32af67ea11045f521c5022bc9f952
bmiranda <brengabriela@gmail.com>
2022-11-23 16:14:37 -0300

    se creo el template de contacto para devs se agrego un favicon cambio la imagen del hero se edito layout para que el admin vaya a la pagina de contacto para devs se agrego el modo oscuro (codigo en js y nuevo  archivo dark.css) se edito content, la presentacion del contenido del post se modifico alerts.css, la ubicacion de la alerta se modifico app, se agrego contact us

--- app/static/js/layout.js
+++ app/static/js/layout.js
@@ -7,9 +7,32 @@ document.addEventListener("DOMContentLoaded", function (event) {
     alertElem.classList.remove('height');
     setTimeout(() => {
       alertElem.style.display = 'none';
-  }, 3000);
+  }, 2500);
 
   }
   
+  const checkbox = document.getElementById('checkbox');
+
+  const is_dark = localStorage.getItem('dark-theme');
+  if (is_dark === 'true') {
+    if (!document.body.classList.contains('dark')) {
+      document.body.classList.add('dark');
+    }
+  } else {
+    if (document.body.classList.contains('dark')) {
+      document.body.classList.remove('dark');
+    }
+  }
+
+  checkbox.addEventListener('change', () => {
+    document.body.classList.toggle('dark');
+    if (document.body.classList.contains('dark')) {
+      localStorage.setItem('dark-theme', 'true')
+    } else {
+      localStorage.setItem('dark-theme', 'false')
+    }
+    
+  })
 
 });
+
