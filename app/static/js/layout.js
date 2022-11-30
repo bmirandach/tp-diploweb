b2a00f6e5c4490d71e5ccda60663dcd79cb7c5b5
Brenda Miranda <45616587+bmirandach@users.noreply.github.com>
2022-11-30 10:35:41 -0300

    menu mobile
    se agregó un menu para pantallas pequeñas
    se modifico el template users (se sacó la clase container, se agregó users)

--- app/static/js/layout.js
+++ app/static/js/layout.js
@@ -1,3 +1,6 @@
+/*--------------------------------------*/
+/* muestra/oculta submenu del nav       */
+/*--------------------------------------*/
 let subMenu = document.getElementById("subMenu");
 
 function toggleMenu()
@@ -7,27 +10,31 @@ function toggleMenu()
 
 document.addEventListener("DOMContentLoaded", function (event) {
   //código a ejecutar cuando el DOM está listo para recibir acciones
-  var alertElem
-  alertElem = document.getElementById('alerts')
-  if (typeof (alertElem) != 'undefined' && alertElem != null) {
-    alertElem.style.display = 'block';
-    alertElem.classList.remove('height');
-    setTimeout(() => {
-      alertElem.style.display = 'none';
-  }, 2500);
 
+  /*--------------------------------------*/
+  /* muestra la alerta por 2.5 seg        */
+  /*--------------------------------------*/
+  const alertElem = document.getElementById('alerts')
+  if (typeof (alertElem) != 'undefined' && alertElem != null) {
+    alertElem.style.display = 'block'
+    alertElem.classList.remove('height')
+    setTimeout(() => {
+      alertElem.style.display = 'none'
+    }, 2500);
   }
   
-  const checkbox = document.getElementById('checkbox');
-
-  const is_dark = localStorage.getItem('dark-theme');
+  /*----------------------------------------------*/
+  /* aplica/quita el tema oscuro y guarda el dato */
+  /*----------------------------------------------*/
+  const checkbox = document.getElementById('checkbox')
+  const is_dark = localStorage.getItem('dark-theme')
   if (is_dark === 'true') {
     if (!document.body.classList.contains('dark')) {
-      document.body.classList.add('dark');
+      document.body.classList.add('dark')
     }
   } else {
     if (document.body.classList.contains('dark')) {
-      document.body.classList.remove('dark');
+      document.body.classList.remove('dark')
     }
   }
 
@@ -38,8 +45,16 @@ document.addEventListener("DOMContentLoaded", function (event) {
     } else {
       localStorage.setItem('dark-theme', 'false')
     }
-    
+  })
+
+  /*--------------------------------------*/
+  /* menu mobile y pequeñas pantallas     */
+  /*--------------------------------------*/
+  const symmenu = document.getElementById('navbar__toggle')
+  const menu = document.getElementById('navbar__menu')
+  symmenu.addEventListener('click', function () {
+    menu.classList.toggle('navbar__menu--open')
+    this.classList.toggle('navbar__toggle--open')
   })
 
 });
-
