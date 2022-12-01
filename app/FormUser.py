7c8a3a57e03d764f2e859bdd3f06ab5ea48b70d6
mas2000 <118755651+mas2000@users.noreply.github.com>
2022-12-01 14:51:30 -0300

    Add files via upload

--- app/FormUser.py
+++ app/FormUser.py
@@ -4,10 +4,22 @@ from wtforms.validators import *
 import email_validator
 
 class UserCreate(FlaskForm):
+    name = StringField("Nombre", validators=[DataRequired(), Length(min= 4)])
+    surname = StringField("Apellido", validators=[DataRequired(), Length(min= 4)])
     username = StringField("Nombre de usuario", validators=[DataRequired(), Length(min= 5)])
     password = PasswordField('Contraseña', validators=[DataRequired(), Length(min=5)])
     email = StringField("Email", validators=[DataRequired(), Email()])
+    imgp =StringField("Foto de perfil(link)", validators=[Optional(), URL()])
     submit = SubmitField("Crear usuario")
+
+class UserUpdate(FlaskForm):
+    name = StringField("Nombre", validators=[DataRequired(), Length(min= 4)])
+    surname = StringField("Apellido", validators=[DataRequired(), Length(min= 4)])
+    username = StringField("Nombre de usuario", validators=[DataRequired(), Length(min= 5)])
+    password = PasswordField('Contraseña', validators=[DataRequired(), Length(min=5)])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    imgp =StringField("Foto de perfil(link)", validators=[Optional(), URL()])
+    submit = SubmitField("Guardar perfil")
 
 class LoginForm(FlaskForm):
     username = StringField('Nombre de usuario', validators=[DataRequired()])
